6f11ec29145d2f79240ff7edbf8acd24b1d403f0
jair.rosete21@gmail.com <jair.rosete21@gmail.com>
2025-06-05 19:14:43 -0600

    subir

---
+++ js/nav-bar.js
@@ -0,0 +1,39 @@
+import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js"
+
+export class NavBar extends HTMLElement {
+
+ connectedCallback() {
+  this.classList.add("md-navigation-bar")
+
+  this.innerHTML = /* HTML */`
+   <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+    <span class="material-symbols-outlined">home</span>
+    Inicio
+   </a>
+
+   <a ${resaltaSiEstasEn(["/archivosycamara.html"])} href="archivosycamara.html">
+   <span class="material-symbols-outlined">
+folder_open
+</span>
+    Archivos y Cámara
+   </a>
+
+   <a id="navbar" ${resaltaSiEstasEn(["/camara.html"])} href="camara.html">
+    <span class="material-symbols-outlined">
+photo_camera
+</span>
+    Cámara
+   </a>
+
+   <a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
+    <span class="material-symbols-outlined">
+support_agent
+</span>
+    Ayuda
+   </a>`
+
+ }
+
+}
+
+customElements.define("nav-bar", NavBar)
